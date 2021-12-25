1da3982603bb08c51e856b31c4fb2c8bfda91538
yeonc <chsynee@gmail.com>
2021-12-25 09:37:19 +0900

    fix: 크롤링해 온 상품 이름, 설명의 문자가 깨지는 문제 해결

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def add_item():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
+    data.encoding = 'utf-8'
 
     soup = BeautifulSoup(data.text, 'html.parser')
 
